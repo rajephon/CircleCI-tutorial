672a374eed3d8760a4eda18e0767991c451570e9
Chanwoo Noh <rajephon@gmail.com>
2018-12-25 12:42:52 +0900

    init

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,34 @@
+using Xunit;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Fact]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void ReturnFalseGivenValuesLessThan2(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
+}
